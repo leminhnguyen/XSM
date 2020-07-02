98e15d949f694aef3b97fbbf2e7d027c06338e9d
leminhnguyen <leminhnguyen.bk@gmail.com>
2020-07-02 12:35:28 +0700

    updated main.py

--- source/main.py
+++ source/main.py
@@ -13,29 +13,7 @@ from RefClassTime import ReferenceClassTime
 
 if __name__ == "__main__":
     datafile = sys.argv[1]
-    # time forcasting
-    print("\n--------- Time Forcasting -----------")
-    plt.style.use("seaborn")
-    # figure, axes = plt.subplots(nrows=2, ncols=2)
-    row, col = 0, 0
-    PROJECTS = ["C2014-05", "C2014-06", "C2014-07", "C2014-08"]
-    prev = []
-    for idx, project in enumerate(PROJECTS):
-        timeforcasting = Time(project)
-        mape, error = timeforcasting.time_forecasting()
-        TPs = [str(x) for x, tp in enumerate(timeforcasting.tracking_periods)]
-        # axes[row][col].set_title(project + f" -- mape={mape: .3f}")
-        # axes[row][col].set_ylabel("Mape Values")
-        # if row==1 and col <= 1: axes[row][col].set_xlabel("Tracking Periods")
-        plt.plot(TPs, np.append(error,0), label=f"{project} - mape={round(mape, 2)}")
-        plt.legend()
-        if (idx+1)%2 == 0: 
-            row += 1; col=0
-        else: col += 1
-        prev.append((TPs, np.append(error,0), f"{project} - mape={round(mape, 2)}"))
-        
-
-    # plt.show()
+   
 
     # dynamic forcasting
     # print("\n-------- Dynamic Forcasting ---------")
@@ -53,11 +31,24 @@ if __name__ == "__main__":
     # evmforcasting.forecast()
 
     # reference class forcasting
+    print("\n--------- Time Forcasting -----------")
+    plt.style.use("seaborn")
+    row, col = 0, 0
+    PROJECTS = ["C2014-05", "C2014-06", "C2014-07", "C2014-08"]
+    prev = []
+    for idx, project in enumerate(PROJECTS):
+        timeforcasting = Time(project)
+        mape, error = timeforcasting.time_forecasting()
+        TPs = [str(x) for x, tp in enumerate(timeforcasting.tracking_periods)]
+        plt.plot(TPs, np.append(error,0), label=f"{project} - mape={round(mape, 2)}")
+        plt.legend()
+        if (idx+1)%2 == 0: 
+            row += 1; col=0
+        else: col += 1
+        prev.append((TPs, np.append(error,0), f"{project} - mape={round(mape, 2)}"))
+
     print("\nReference Class Forcasting")
     PROJECTS = ["C2014-05", "C2014-06", "C2014-07", "C2014-08"]
-    # for project in PROJECTS:
-    #     rc = ReferenceClassTime(project)
-    #     rc.get_best_beta()
     rc = ReferenceClassTime(datafile)
     res = rc.time_forecasting()
 
